2c9794a317c2a304907054bc1c5354303d98edd4
Vector Miller <victorglebovv@yandex.ru>
2022-08-19 19:10:57 +0500

    Landing done

--- js/main.js
+++ js/main.js
@@ -9,7 +9,9 @@
 //   document.body.classList.toggle('no-scroll');
 // }
 
+
 /* Changing color of arrow on button when hover is active */
+
 let nav_button = document.querySelector(".nav__button");
 
 function setAttributes(el, attrs) {
@@ -42,3 +44,51 @@ nav_button.addEventListener('mouseleave', () => {
     image_arrow_light.remove();
     nav_button.append(image_arrow_dark);
 });
+
+
+/* Carousel Script */
+
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        dots: false,
+        margin: 48,
+        loop: true,
+    });
+});
+
+/* Video-Button Script */
+
+const videoBtn = document.querySelector('#video-btn');
+const videoPrev = document.querySelector('.video__preview');
+const videoWrapper = document.querySelector('.video__cover');
+const video = document.querySelector('#video-file');
+
+videoWrapper.addEventListener('click', function() {
+    if (video.paused) {
+        videoWrapper.classList.remove('video-overlay');
+        videoPrev.classList.add('none');
+        videoBtn.classList.add('none');
+        video.play()
+    } else {
+        videoWrapper.classList.add('video-overlay');
+        videoBtn.classList.remove('none');
+        video.pause()
+    }
+})
+
+document.body.onkeydown = function(e) {
+    if (e.keyCode == '32') {
+        if (videoPrev.classList.contains('none')) {
+            if (video.paused) {
+                videoWrapper.classList.remove('video-overlay');
+                videoPrev.classList.add('none');
+                videoBtn.classList.add('none');
+                video.play()
+            } else {
+                videoWrapper.classList.add('video-overlay');
+                videoBtn.classList.remove('none');
+                video.pause()
+            }
+        }
+    }
+};
